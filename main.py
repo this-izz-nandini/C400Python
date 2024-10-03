f0486ed5e51e06d95dba5a3361b9e6441e6a73f4
Nandini Jindal <nandinijindal321@gmail.com>
2024-10-03 11:26:49 +0530

    commented a part

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import requests
 
 CSV_URL = 'https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv'
 
-with requests.get(CSV_URL, stream=True) as r:
-    lines = (line.decode('utf-8') for line in r.iter_lines())
+with requests.get(CSV_URL, stream=True) as file:
+    lines = (line.decode('utf-8') for line in file.iter_lines())
     data = [row for row in csv.reader(lines)]
 
 # print(data)
-total=len(data)
+total = len(data) - 1
 print(f'total no. of records: {total}')
 
 borough=[]
@@ -25,7 +25,7 @@ print(f'\n\nunique boroughs:\n {borough}')
 
 print(f'\n\nBrooklyn boroughs: {brooks}')
 
-with open('/root/taxi_zone_output.txt', mode='w') as op:
-    op.write(total)
-    op.write(borough)
-    op.write(brooks)
+# with open('/root/taxi_zone_output.txt', mode='w') as op:
+#     op.write(f'total no. of records: {total}')
+#     op.write(f'\n\nunique boroughs:\n {borough}')
+#     op.write(f'\n\nBrooklyn boroughs: {brooks}')
